c6c17e53868a8f59e87a5db3ccdcc34584c1079b
Olamide Osuolale <oluwajuedaloade@gmail.com>
2023-09-10 01:09:26 +0100

    create a get endpoint

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/api', (req, res) => {
+    const {slack_name, track} = req.query;
+
+    const timeNow = new Date();
+
+    const dateToday = timeNow.toLocaleDateString('en-US', {weekday: 'long'});
+    const utcTime = timeNow.toISOString();
+
+    const response = {
+        slack_name,
+        current_day: dateToday,
+        utc_time: utcTime,
+        track,
+        github_file_url: '',
+        github_repo_url:'',
+        status_code: 200,
+    }
+
+    res.json(response);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
